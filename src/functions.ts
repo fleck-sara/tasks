11cc99b85376094f451b4dc312cb22b9572737e6
fleck-sara <sfleck@udel.edu>
2022-02-16 12:37:24 -0500

    completed fn shout

--- src/functions.ts
+++ src/functions.ts
@@ -30,7 +30,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    message = message.toUpperCase();
+    message = message + "!";
+    return message;
 }
 
 /**
