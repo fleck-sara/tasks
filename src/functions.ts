d269c37aa447928f3823c9342f1dafdd1d596dfb
fleck-sara <sfleck@udel.edu>
2022-02-16 12:35:14 -0500

    completed f->C and add3 fns

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let s = 0;
+    if (first > 0) {
+        s = s + first;
+    }
+    if (second > 0) {
+        s = s + second;
+    }
+    if (third > 0) {
+        s = s + third;
+    }
+    return s;
 }
 
 /**
