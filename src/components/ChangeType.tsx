0d8e5752e34836727738705e081f4e3de6c34fd3
fleck-sara <sfleck@udel.edu>
2022-03-07 12:01:28 -0500

    first 3 problems

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,27 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [value, setValue] = useState<QuestionType>("short_answer_question");
+    //return <div>Change Type</div>;
+    function flipType(): void {
+        if (value === "multiple_choice_question") {
+            setValue("short_answer_question");
+        } else {
+            setValue("multiple_choice_question");
+        }
+    }
+    return (
+        <>
+            <div>
+                <Button onClick={flipType}>Change Type</Button>
+            </div>
+            <div>
+                {value === "short_answer_question" ? (
+                    <span>Short Answer</span>
+                ) : (
+                    <span>Multiple Choice</span>
+                )}
+            </div>
+        </>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    //return <div>Start Attempt</div>;
+    const [numAttempts, setnumAttempts] = useState<number>(4);
+    const [inProgress, setinProgress] = useState<boolean>(false);
+    function startquiz(): void {
+        setinProgress(true);
+        setnumAttempts(numAttempts - 1);
+    }
+    return (
+        <div>
+            <div>
+                <Button
+                    onClick={() => setnumAttempts(1 + numAttempts)}
+                    disabled={inProgress}
+                >
+                    Mulligan
+                </Button>
+                number of attempts: {numAttempts}.
+            </div>
+            <div>
+                <Button
+                    onClick={() => startquiz()}
+                    disabled={inProgress || numAttempts === 0}
+                >
+                    Start Quiz
+                </Button>
+                <Button
+                    onClick={() => setinProgress(false)}
+                    disabled={!inProgress}
+                >
+                    Stop Quiz
+                </Button>
+            </div>
+        </div>
+    );
 }
