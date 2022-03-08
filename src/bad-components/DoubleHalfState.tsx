12218da14807c806c3da7174cd06588fd73fa753
fleck-sara <sfleck@udel.edu>
2022-03-07 20:01:16 -0500

    finished doubleHalf

--- src/bad-components/DoubleHalfState.tsx
+++ src/bad-components/DoubleHalfState.tsx
@@ -1,3 +1,7 @@
-import { useState } from "react";
+//import { useState } from "react";
+export interface doubleProps {
+    dhValue: number;
+    setDhValue: (newDhValue: number) => void;
+}
 
-export const [dhValue, setDhValue] = useState<number>(10);
+//export const [dhValue, setDhValue] = useState<number>(10);
