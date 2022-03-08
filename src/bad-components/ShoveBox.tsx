02a2480ec6442724a1f5b61ab95d50b8e3037fb7
fleck-sara <sfleck@udel.edu>
2022-03-08 00:08:40 -0500

    finished shoveBox

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface BoxProps {
+    position: number;
+    setPosition: (newPosition: number) => void;
+}
 function ShoveBoxButton({
     position,
     setPosition
@@ -13,8 +17,7 @@ function ShoveBoxButton({
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position }: BoxProps): JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +35,24 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
+    const [position, setPosition] = useState<number>(10);
 
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
-                {box}
-            </div> */}
+            </div>
+            <div>
+                <MoveableBox
+                    position={position}
+                    setPosition={setPosition}
+                ></MoveableBox>
+            </div>
         </div>
     );
 }
