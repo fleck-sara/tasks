42bd3b6cabf69930ef6f5440cd4dc5ac178cbf9b
fleck-sara <sfleck@udel.edu>
2022-03-16 18:56:59 -0400

    quiz viewable, clicking shows questions

---
+++ src/quizzer/QuestionView.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Question } from "../interfaces/question";
+export function QuestionView({
+    question
+}: {
+    question: Question;
+}): JSX.Element {
+    function isMultChoice(): boolean {
+        return question.type === "multiple_choice_question";
+    }
+    const qtype = isMultChoice();
+    return qtype ? (
+        <Container>
+            <Row>
+                <Col>
+                    <h4>Title: {question.name}</h4>
+                    <h6>{question.body} </h6>
+                    <i>Points: {question.points}</i>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <h6>The Answer options are:</h6>
+                    {question.options.map((o: string) => (
+                        <div key={question.id}>
+                            <h6>{o}</h6>
+                        </div>
+                    ))}
+                </Col>
+            </Row>
+        </Container>
+    ) : (
+        <Container>
+            <Row>
+                <Col>
+                    <h4>Title: {question.name}</h4>
+                    <h6>{question.body} </h6>
+                    <i>Points: {question.points}</i>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>open ended question</p>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
--- src/quizzer/QuizList.tsx
+++ src/quizzer/QuizList.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import { Quiz } from "../interfaces/quiz";
 import { QuizView } from "./QuizView";
 import { Stack } from "react-bootstrap";
+import { QuestionList } from "./QuestionList";
 
 export function QuizList({ quizzes }: { quizzes: Quiz[] }): JSX.Element {
-    return (
-        <Stack gap={3}>
-            {quizzes.map((quiz: Quiz) => (
-                <div key={quiz.id}>
-                    <QuizView quiz={quiz}></QuizView>
-                </div>
-            ))}
-        </Stack>
+    const [viewqs, setviewqs] = useState<boolean>(false);
+    function changeView() {
+        setviewqs(!viewqs);
+    }
+    return viewqs ? (
+        <QuestionList
+            questions={quizzes[0].questions}
+            changeView={changeView}
+        ></QuestionList>
+    ) : (
+        <>
+            <Stack gap={3} onClick={changeView}>
+                {quizzes.map((quiz: Quiz) => (
+                    <div key={quiz.id}>
+                        <QuizView quiz={quiz}></QuizView>
+                    </div>
+                ))}
+            </Stack>
+        </>
     );
 }
