a3d4ceee6d7f8827c443c395ae6d356a810922c5
fleck-sara <sfleck@udel.edu>
2022-03-17 14:18:49 -0400

    working on ability to take a quiz

--- src/quizzer/QuizView.tsx
+++ src/quizzer/QuizView.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { Quiz } from "../interfaces/quiz";
+import { useState } from "react";
+import { Takequiz } from "./TakeQuiz";
+import { Question } from "../interfaces/question";
 
 export function QuizView({ quiz }: { quiz: Quiz }): JSX.Element {
-    return (
+    const [takequiz, settakequiz] = useState<boolean>(false);
+    function changetakequiz() {
+        settakequiz(!takequiz);
+    }
+    const questions: Question[] = quiz.questions;
+    return takequiz ? (
+        <div>
+            {questions.map((question: Question) => (
+                <div key={question.id}>
+                    <Takequiz
+                        options={question.options}
+                        expectedanswer={question.expected}
+                    ></Takequiz>
+                </div>
+            ))}
+        </div>
+    ) : (
         <Container>
             <Row>
                 <Col>
@@ -14,6 +33,18 @@ export function QuizView({ quiz }: { quiz: Quiz }): JSX.Element {
             <Row>
                 <Col>
                     <p>Length: {quiz.questions.length}</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button
+                        className="float-right me-3"
+                        size="sm"
+                        variant="success"
+                        onClick={() => changetakequiz()}
+                    >
+                        Take Quiz
+                    </Button>
                 </Col>
             </Row>
         </Container>
